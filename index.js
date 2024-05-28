9504370d20fb72fc0dd9ee51bbbfd0d1bca0497d
Anshuman <107368860+anshuman008@users.noreply.github.com>
2024-05-29 04:08:21 +0530

    env added

--- index.js
+++ index.js
@@ -4,13 +4,18 @@ const log = require('./routes/log');
 const app = express();
 const PORT = 8080;
 const cors = require('cors');
+const dotenv = require('dotenv')
+
+
+dotenv.config();
+
 // Middleware to parse JSON bodies
 app.use(express.json());
 app.use(cors());
 // Connect to the database
 async function connectDB() {
     try {
-        await mongoose.connect('mongodb://127.0.0.1:27017/logs');
+        await mongoose.connect(process.env.MONGO_URL);
         console.log("Connected to DB");
     } catch (error) {
         console.error("Failed to connect to DB", error);
